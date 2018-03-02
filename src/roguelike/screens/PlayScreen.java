6a9a3a78f7e3cc0e49da72fa6ee5b2ca7ba6a98b
rwm6857 <ryan.moore7292@gmail.com>
2018-03-02 18:28:26 -0500

    Added scrolling and movement of caves to PlayScreen
    Signed-off-by: Ryan Moore <ryan.moore7292@gmail.com>

--- src/roguelike/screens/PlayScreen.java
+++ src/roguelike/screens/PlayScreen.java
@@ -52,11 +52,35 @@ public class PlayScreen implements Screen {
     }
 
     /**
+     * displays tiles on the terminal
+     *
+     * @param terminal terminal window
+     * @param left     left most position
+     * @param top      top most position
+     */
+    private void displayTiles(AsciiPanel terminal, int left, int top) {
+        for (int x = 0; x < screenWidth; x++) {
+            for (int y = 0; y < screenHeight; y++) {
+                int wx = x + left;
+                int wy = y + top;
+
+                terminal.write(world.glyph(wx, wy), x, y, world.color(wx, wy));
+            }
+        }
+    }
+
+    /**
      * displays gameplay on the terminal
      *
      * @param terminal the AsciiPanel to deliver output
      */
     public void displayOutput(AsciiPanel terminal) {
+        int left = getScrollX();
+        int top = getScrollY();
+
+        displayTiles(terminal, left, top);
+        terminal.write('X', centerX - left, centerY - top);
+
         terminal.write("You are having fun.", 1, 1);
         terminal.writeCenter(" ~~ press [ESC] to lose or [ENTER] to win ~~", 22);
     }
@@ -73,7 +97,65 @@ public class PlayScreen implements Screen {
                 return new LoseScreen();
             case KeyEvent.VK_ENTER:
                 return new WinScreen();
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_H:
+                scrollBy(-1, 0);
+                break;
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_L:
+                scrollBy(1, 0);
+                break;
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_K:
+                scrollBy(0, -1);
+                break;
+            case KeyEvent.VK_DOWN:
+            case KeyEvent.VK_J:
+                scrollBy(0, 1);
+                break;
+            case KeyEvent.VK_Y:
+                scrollBy(-1, -1);
+                break;
+            case KeyEvent.VK_U:
+                scrollBy(1, -1);
+                break;
+            case KeyEvent.VK_B:
+                scrollBy(-1, 1);
+                break;
+            case KeyEvent.VK_N:
+                scrollBy(1, 1);
+                break;
         }
         return this;
     }
+
+    /**
+     * limits how far we can scroll in x
+     *
+     * @return maximum x value
+     */
+    public int getScrollX() {
+        return Math.max(0, Math.min(centerX - screenWidth / 2, world.width() - screenWidth));
+    }
+
+    /**
+     * limits how far we can scroll in y
+     *
+     * @return maximum y value
+     */
+    public int getScrollY() {
+        return Math.max(0, Math.min(centerY - screenHeight / 2, world.height() - screenHeight));
+    }
+
+    /**
+     * makes sure we aren't scrolling out of bounds
+     *
+     * @param mx max x
+     * @param my max y
+     */
+    private void scrollBy(int mx, int my) {
+        centerX = Math.max(0, Math.min(centerX + mx, world.width() - 1));
+        centerY = Math.max(0, Math.min(centerY + my, world.height() - 1));
+    }
+
 }
