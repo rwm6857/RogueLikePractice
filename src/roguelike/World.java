154a75073dd462a540a24854137c22caea68dc20
rwm6857 <ryan.moore7292@gmail.com>
2018-03-02 17:51:34 -0500

    Created methods that allow us to access attributes of the World
    Signed-off-by: Ryan Moore <ryan.moore7292@gmail.com>

--- src/roguelike/World.java
+++ src/roguelike/World.java
@@ -2,6 +2,11 @@ package roguelike;
 
 import java.awt.Color;
 
+/**
+ * Creates the world and gives access to it's attributes
+ *
+ * @author rwm6857
+ */
 public class World {
     /**
      * 2D array for storing tiles
@@ -43,6 +48,42 @@ public class World {
      */
     public int height() {
         return height;
+    }
+
+    /**
+     * checks for bounds
+     *
+     * @param x horizontal position
+     * @param y vertical position
+     * @return either returns BOUNDS or the tile at x and y
+     */
+    public Tile tile(int x, int y) {
+        if (x < 0 || x >= width || y < 0 || y >= height)
+            return Tile.BOUNDS;
+        else
+            return tiles[x][y];
+    }
+
+    /**
+     * gets the glyph at the current position
+     *
+     * @param x horizontal position
+     * @param y vertical position
+     * @return glyph at (x,y)
+     */
+    public char glyph(int x, int y) {
+        return tile(x, y).glyph();
+    }
+
+    /**
+     * gets the color at the current position
+     *
+     * @param x horizontal position
+     * @param y vertical position
+     * @return color at (x,y)
+     */
+    public Color color(int x, int y) {
+        return tile(x, y).color();
     }
 }
 
