99c5e90043bce9d9d8896a74995ef1a0f030c00e
rwm6857 <ryan.moore7292@gmail.com>
2018-03-02 17:26:29 -0500

    Implemented the KeyListener interface in ApplicationMain to allow screen changes
    Signed-off-by: Ryan Moore <ryan.moore7292@gmail.com>

--- src/roguelike/ApplicationMain.java
+++ src/roguelike/ApplicationMain.java
@@ -3,6 +3,12 @@ package roguelike;
 import javax.swing.JFrame;
 
 import asciiPanel.AsciiPanel;
+import roguelike.screens.Screen;
+import roguelike.screens.StartScreen;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 
 /**
  * Main application class for my first RogueLike, following a tutorial
@@ -15,7 +21,7 @@ import asciiPanel.AsciiPanel;
  *
  * @author rwm6857 (Ryan Moore)
  */
-public class ApplicationMain extends JFrame {
+public class ApplicationMain extends JFrame implements KeyListener {
     /**
      * helps to prevent faliues when serializing different class versions
      */
@@ -24,6 +30,10 @@ public class ApplicationMain extends JFrame {
      * terminal window to display the game info
      */
     private AsciiPanel terminal;
+    /**
+     * the Screen currently being displayed
+     */
+    private Screen screen;
 
     /**
      * creates the ApplicationMain window
@@ -34,12 +44,51 @@ public class ApplicationMain extends JFrame {
         terminal.write("RogueLike Tutorial", 1, 1);
         add(terminal);
         pack();
+        screen = new StartScreen();
+        addKeyListener(this);
+        repaint();
     }
+
+    /**
+     * rebuilds the terminal window
+     */
+    public void repaint() {
+        terminal.clear();
+        screen.displayOutput(terminal);
+        super.repaint();
+    }
+
+    /**
+     * checks if a key is pressed and repaints the screen
+     *
+     * @param e key event code
+     */
+    public void keyPressed(KeyEvent e) {
+        screen = screen.respondToUserInput(e);
+        repaint();
+    }
+
+    /**
+     * checks if key is released
+     *
+     * @param e key event code
+     */
+    public void keyReleased(KeyEvent e) {
+    }
+
+    /**
+     * checks if a key is typed
+     *
+     * @param e key event code
+     */
+    public void keyTyped(KeyEvent e) {
+    }
+
 
     /**
      * main function, instantiates ApplicationMean and delegates input
      *
-     * @param args command-line arguements
+     * @param args command-line arguments
      */
     public static void main(String[] args) {
         ApplicationMain app = new ApplicationMain();
