ec1f751cd63ccca5d7835a0ce5198e8f86c1201f
Ryan Moore <ryan.moore7292@gmail.com>
2018-03-02 17:04:21 -0500

    Implemented StartScreen.java
    Signed-off-by: Ryan Moore <ryan.moore7292@gmail.com>

---
+++ src/roguelike/screens/StartScreen.java
@@ -0,0 +1,31 @@
+package roguelike.screens;
+
+import asciiPanel.AsciiPanel;
+
+import java.awt.event.KeyEvent;
+
+/**
+ * Provides the starting screen for the game
+ * Puts user in "play mode" after hitting enter
+ */
+public class StartScreen implements Screen {
+    /**
+     * Prints the opening message for the StartScreen
+     *
+     * @param terminal the AsciiPanel to deliver output
+     */
+    public void displayOutput(AsciiPanel terminal) {
+        terminal.write("RogueLike Tutorial", 1, 1);
+        terminal.writeCenter("~~ Press [ENTER] to start ~~", 22);
+    }
+
+    /**
+     * Invokes the PlayScreen when the user hits enter
+     *
+     * @param key value that triggers an event
+     * @return
+     */
+    public Screen respondToUserInput(KeyEvent key) {
+        return key.getKeyCode() == KeyEvent.VK_ENTER ? PlayScreen() : this;
+    }
+}
