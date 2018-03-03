7e26f360566323cd93e2b5102fb8036a59ba5ac6
rwm6857 <ryan.moore7292@gmail.com>
2018-03-03 14:21:39 -0500

    Fixed issue with player not being able to move: removed scrolling methods and attributes and gave control to PlayerAi
    Signed-off-by: Ryan Moore <ryan.moore7292@gmail.com>

--- src/roguelike/screens/PlayScreen.java
+++ src/roguelike/screens/PlayScreen.java
@@ -20,17 +20,6 @@ public class PlayScreen implements Screen {
      * generated world
      */
     private World world;
-    /**
-     * center of the screen horizontally
-     */
-    private int centerX;
-    /**
-     * center of the screen vertically
-     */
-    private int centerY;
-    /**
-     * width of the screen
-     */
     private int screenWidth;
     /**
      * height of the screen
@@ -107,31 +96,31 @@ public class PlayScreen implements Screen {
                 return new WinScreen();
             case KeyEvent.VK_LEFT:
             case KeyEvent.VK_H:
-                scrollBy(-1, 0);
+                player.moveBy(-1, 0);
                 break;
             case KeyEvent.VK_RIGHT:
             case KeyEvent.VK_L:
-                scrollBy(1, 0);
+                player.moveBy(1, 0);
                 break;
             case KeyEvent.VK_UP:
             case KeyEvent.VK_K:
-                scrollBy(0, -1);
+                player.moveBy(0, -1);
                 break;
             case KeyEvent.VK_DOWN:
             case KeyEvent.VK_J:
-                scrollBy(0, 1);
+                player.moveBy(0, 1);
                 break;
             case KeyEvent.VK_Y:
-                scrollBy(-1, -1);
+                player.moveBy(-1, -1);
                 break;
             case KeyEvent.VK_U:
-                scrollBy(1, -1);
+                player.moveBy(1, -1);
                 break;
             case KeyEvent.VK_B:
-                scrollBy(-1, 1);
+                player.moveBy(-1, 1);
                 break;
             case KeyEvent.VK_N:
-                scrollBy(1, 1);
+                player.moveBy(1, 1);
                 break;
         }
         return this;
@@ -143,7 +132,7 @@ public class PlayScreen implements Screen {
      * @return maximum x value
      */
     public int getScrollX() {
-        return Math.max(0, Math.min(centerX - screenWidth / 2, world.width() - screenWidth));
+        return Math.max(0, Math.min(player.x - screenWidth / 2, world.width() - screenWidth));
     }
 
     /**
@@ -152,18 +141,6 @@ public class PlayScreen implements Screen {
      * @return maximum y value
      */
     public int getScrollY() {
-        return Math.max(0, Math.min(centerY - screenHeight / 2, world.height() - screenHeight));
+        return Math.max(0, Math.min(player.y - screenHeight / 2, world.height() - screenHeight));
     }
-
-    /**
-     * makes sure we aren't scrolling out of bounds
-     *
-     * @param mx max x
-     * @param my max y
-     */
-    private void scrollBy(int mx, int my) {
-        centerX = Math.max(0, Math.min(centerX + mx, world.width() - 1));
-        centerY = Math.max(0, Math.min(centerY + my, world.height() - 1));
-    }
-
 }
